0513916a37bc9f8a643273e488d8251a26978bd7
Reza <hungry.dev39@gmail.com>
2021-02-02 22:19:19 +0700

    implement logout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,18 @@
 import React  from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Box, Button, HStack, Spacer, useColorMode } from '@chakra-ui/react';
 import { SunIcon } from '@chakra-ui/icons';
 import useSwr from 'swr';
 import Users from './Users';
 import fetcher from '../utility/fetcher';
+import { useAuth } from './Auth';
 
 export default function Dashboard() {
   const { data, error } = useSwr('/api/user', fetcher);
 
+  const history = useHistory();
   const { toggleColorMode } = useColorMode();
+  const [auth, setAuth] = useAuth();
 
   if (error) return 'Error'
   if (!data) return 'Loading...'
@@ -21,6 +24,10 @@ export default function Dashboard() {
         <Button as={Link} to="/similar" >Similar</Button>
         <Spacer />
         <Button onClick={toggleColorMode}><SunIcon /></Button>
+        <Button onClick={() => {
+          setAuth('')
+          history.push('/login');
+        }}>Logout</Button>
       </HStack>
       {data.users.map(user => <Users {...user} />)}
     </Box>
